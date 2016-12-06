bc3733fe94a459438f4ceff06b8fd462f749c708
jandornig <jandornig@gmail.com>
2016-12-06 15:53:01 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -26,6 +26,6 @@ function draw() {
     var touch = touches[i];
     var touchColor = colorList[i];
     fill(colorList[i]);
-    rect(touch.x, touch.y, 150, 150);
+    ellipse(touch.x, touch.y, 150, 150);
   }
 }
